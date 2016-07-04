82cf33db0a7413bf1448f67ae26ccca9cd8f3fa7
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2016-07-04 21:01:00 +0200

    core: platform-generic: give root window it's own class

--- src/platform/generic.c
+++ src/platform/generic.c
@@ -3,11 +3,13 @@
 #include <cairo.h>
 #include <cairo-drm.h>
 #include <twtk/types.h>
+#include <twtk/widget-helper.h>
 #include <twtk/platform.h>
 #include <twtk/widget.h>
 #include <twtk-private/platform-generic.h>
 #include <twtk/widgets.h>
 #include <twtk/color.h>
+
 
 cairo_t *_twtk_platform_generic_get_context(twtk_platform_t *pl)
 {
@@ -23,29 +25,50 @@ int _twtk_platform_generic_free_context(twtk_platform_t *pl, cairo_t *ctx)
     return 1;
 }
 
+#define MAGIC			0x4f52544f30303030
+
+#define _WIDGET_PRIV_TYPE	_root_priv_t
+#define _WIDGET_CLASS_INF	_root_class_inf
+
+static int _op_event(twtk_widget_t *widget, twtk_event_t *event, twtk_event_dispatch_t d)
+    __attribute__((nonnull(1,2)));
+
+typedef struct
+{
+} _root_priv_t;
+
+static twtk_widget_class_t _root_class_inf = {
+    .magic     = MAGIC,
+    .priv_size = sizeof(_root_priv_t)
+};
+
+static twtk_widget_t *_create_root(cairo_surface_t *surface)
+{
+    assert(surface);
+
+    twtk_widget_t *r = twtk_widget_alloc(&_root_class_inf);
+    assert(r);
+
+    r->viewport.pos = TWTK_VECTOR(0,0);
+
+    /** fixme: we should add surface-type independent functions in cairo **/
+    r->viewport.size = TWTK_VECTOR(
+        cairo_drm_surface_get_width(surface),
+        cairo_drm_surface_get_height(surface)
+    );
+
+    twtk_widget_set_name(r, "<ROOT>");
+    twtk_widget_set_background_color(r, TWTK_COLOR_BLACK);
+
+    return r;
+}
+
 twtk_widget_t *_twtk_platform_generic_get_root(twtk_platform_t *platform)
 {
     assert(platform);
 
     if (platform->root == NULL)
-    {
-	assert(platform->surface);
-
-	platform->root = twtk_text_widget_create(
-	    NULL,
-	    0,
-	    0,
-	    /** fixme: we should add surface-type independent functions in cairo **/
-	    cairo_drm_surface_get_width(platform->surface),
-	    cairo_drm_surface_get_height(platform->surface));
-
-	twtk_widget_set_name(platform->root, "<ROOT>");
-
-	twtk_widget_set_background_color(
-	    platform->root,
-	    (twtk_color_t){.alpha = 0xff, .red = 0x15, .green = 0x22, .blue = 0x33}
-	);
-    }
+        platform->root = _create_root(platform->surface);
 
     return platform->root;
 }
