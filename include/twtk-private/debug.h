f0a76159704de0069fc3cdff64b79f1f8f5006ad
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2016-07-10 21:00:48 +0200

    core: debug.h: fixed missing include

--- include/twtk-private/debug.h
+++ include/twtk-private/debug.h
@@ -1,6 +1,8 @@
 #ifndef __TWTK_PRIVATE_DEBUG
 #define __TWTK_PRIVATE_DEBUG
 
+#include <stdio.h>
+
 #ifdef ENABLE_DEBUG
 #define _DEBUG(text, ...)       { fprintf(stderr, "[twtk/" _DEBUG_NAME "] " text "\n", ##__VA_ARGS__); }
 #else
