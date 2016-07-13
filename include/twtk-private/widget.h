5d8aeefc00887c010fcbf9768e22456e34235057
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2016-07-13 10:59:35 +0200

    core: position translation in both directions

--- include/twtk-private/widget.h
+++ include/twtk-private/widget.h
@@ -4,11 +4,24 @@
 #include <cairo.h>
 #include <twtk/types.h>
 
+
 cairo_matrix_t _twtk_widget_calc_matrix(const twtk_widget_t *widget);
 cairo_matrix_t _twtk_widget_calc_inv_matrix(const twtk_widget_t *widget);
 
-int twtk_widget_translate_pos(const twtk_widget_t *widget,
+/**
+ * translate a position from parent frame into widget coordinate system
+ */
+int twtk_widget_translate_pos_from_frame(const twtk_widget_t *widget,
 			      twtk_vector_t pos,
-			      twtk_vector_t *ret_pos);
+			      twtk_vector_t *ret_pos)
+    __attribute__((nonnull(1,3)));
+
+/**
+ * translate a position from widget coordinate system to parent frame
+ */
+int twtk_widget_translate_pos_to_frame(const twtk_widget_t *widget,
+			      twtk_vector_t pos,
+			      twtk_vector_t *ret_pos)
+    __attribute__((nonnull(1,3)));
 
 #endif /* __TWTK_PRIVATE_WIDGET_H */
