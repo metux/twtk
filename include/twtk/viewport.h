ecfe66714c0707202d403d0196e4058c9dab29f0
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2016-07-20 22:47:11 +0200

    core: caching widget transformation matrices

--- include/twtk/viewport.h
+++ include/twtk/viewport.h
@@ -5,19 +5,37 @@
 #include <cairo.h>
 #include <twtk/types.h>
 #include <twtk/rect.h>
+#include <twtk/widget.h>
 
 
-void twtk_viewport_matrix(const twtk_rect_t viewport, cairo_matrix_t *matrix)
-    __attribute__((nonnull(2)));
+cairo_matrix_t *_twtk_do_viewport_matrix(twtk_widget_t *widget)
+    __attribute__((nonnull(1)));
 
-void twtk_viewport_inv_matrix(const twtk_rect_t viewport, cairo_matrix_t *matrix)
-    __attribute__((nonnull(2)));
+cairo_matrix_t *_twtk_do_viewport_inv_matrix(twtk_widget_t *widget)
+    __attribute__((nonnull(1)));
+
+static inline cairo_matrix_t *twtk_viewport_matrix(twtk_widget_t *widget)
+{
+    if (widget->flags & TWTK_WIDGET_FLAG_HAS_MATRIX)
+        return &widget->matrix;
+
+    return _twtk_do_viewport_matrix(widget);
+}
+
+static inline cairo_matrix_t *twtk_viewport_inv_matrix(twtk_widget_t *widget)
+{
+    if (widget->flags & TWTK_WIDGET_FLAG_HAS_INV_MATRIX)
+        return &widget->inv_matrix;
+
+    return _twtk_do_viewport_inv_matrix(widget);
+}
+
 
 /**
  * translate a position from parent frame into viewport coordinate system
  */
 int twtk_viewport_pos_from_frame(
-    const twtk_rect_t viewport,
+    twtk_widget_t *widget,
     twtk_vector_t pos,
     twtk_vector_t *ret_pos)
     __attribute__((nonnull(3)));
@@ -26,7 +44,7 @@ int twtk_viewport_pos_from_frame(
  * translate a position from widget coordinate system to parent frame
  */
 int twtk_viewport_pos_to_frame(
-    const twtk_rect_t viewport,
+    twtk_widget_t *widget,
     twtk_vector_t pos,
     twtk_vector_t *ret_pos)
     __attribute__((nonnull(3)));
