9c9f70d07e4ab6d765c67d505e5d78fbb7436ed7
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2016-07-20 14:55:29 +0200

    platform: moved redraw operation to platform ops

--- include/twtk-private/platform-generic.h
+++ include/twtk-private/platform-generic.h
@@ -7,6 +7,7 @@
 cairo_t       *_twtk_platform_generic_get_context(twtk_platform_t *pl);
 int            _twtk_platform_generic_free_context(twtk_platform_t *pl, cairo_t *ctx);
 twtk_widget_t *_twtk_platform_generic_get_root(twtk_platform_t *pl);
+int            _twtk_platform_generic_redraw(twtk_platform_t *pl);
 
 /** map a widget into parent frame **/
 int _twtk_platform_generic_map_widget(
