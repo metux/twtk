49f150f96f0cb28b1e9fc40f5b1c7fd642a66723
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2017-04-13 10:58:50 +0200

    widgets: image: pass dimensions via twtk_rect_t

--- include/twtk/widgets/image.h
+++ include/twtk/widgets/image.h
@@ -6,10 +6,7 @@
 
 twtk_widget_t *twtk_image_widget_create(
     const char* filename,
-    twtk_dim_t x,
-    twtk_dim_t y,
-    twtk_dim_t w,
-    twtk_dim_t h
+    twtk_rect_t rect
 );
 
 #endif /* __TWTK_WIDGET_IMAGE_H */
