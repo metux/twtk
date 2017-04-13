ffa061ad46412ee1d49212ad82d6c85de46259cc
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2017-04-13 11:33:58 +0200

    platform: generic: initalize surface size

--- src/platform/generic.c
+++ src/platform/generic.c
@@ -77,6 +77,8 @@ static twtk_widget_t *_create_root(twtk_platform_t *platform, cairo_surface_t *s
         );
     }
 
+    r->surface = r->viewport;
+
     /** fixme: we should add surface-type independent functions in cairo **/
 
     twtk_widget_set_name(r, "<ROOT>");
