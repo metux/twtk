e5a6896d7e5879dc2f779839c83d3971b6083853
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2017-04-27 07:56:18 +0200

    widgets: button: allow setting border color

--- src/widgets/button-widget.c
+++ src/widgets/button-widget.c
@@ -43,6 +43,7 @@ static int _op_paint(twtk_widget_t *widget, cairo_t *cr);
 static int _op_event(twtk_widget_t *widget, twtk_event_t *event, twtk_event_dispatch_t d);
 static int _op_set_float(twtk_widget_t *widget, const char* name, double value);
 static int _op_set_uint(twtk_widget_t *widget, const char* name, uint32_t value);
+static int _op_set_color(twtk_widget_t *widget, const char* name, twtk_color_t value);
 
 static const twtk_widget_class_t _class_inf = {
     .magic        = MAGIC,
@@ -51,6 +52,7 @@ static const twtk_widget_class_t _class_inf = {
     .op_paint     = _op_paint,
     .op_set_float = _op_set_float,
     .op_set_uint  = _op_set_uint,
+    .op_set_color = _op_set_color,
     .priv_size    = sizeof(_priv_t)
 };
 
@@ -75,7 +77,7 @@ static int _op_paint(twtk_widget_t *widget, cairo_t *cr)
     /* paint border */
     if (priv->down)
     {
-        _twtk_ut_set_rgba (cr, FRAME_COLOR);
+        _twtk_ut_set_rgba (cr, priv->border_color);
         cairo_set_line_width (cr, priv->border_width);
         _twtk_ut_rect_to_vec (cr, widget->viewport.size);
         cairo_stroke (cr);
@@ -143,6 +145,13 @@ static int _op_event(twtk_widget_t *widget, twtk_event_t *event, twtk_event_disp
     return 0;
 }
 
+static int _op_set_color(twtk_widget_t *widget, const char* name, twtk_color_t value)
+{
+    TWTK_WIDGET_SET_BEGIN
+    TWTK_WIDGET_SET_COLOR_ATTR("border-color",  border_color);
+    TWTK_WIDGET_SET_END
+}
+
 static int _op_set_float(twtk_widget_t *widget, const char* name, double value)
 {
     TWTK_WIDGET_SET_BEGIN
@@ -172,6 +181,7 @@ twtk_widget_t *twtk_button_widget_create(const char* fn, twtk_rect_t rect, const
 
     priv->image = img;
     priv->border_width = FRAME_SIZE;
+    priv->border_color = FRAME_COLOR;
     STRBUF_SET(priv->signal, signal);
 
     twtk_widget_set_viewport(
